5addb880f53c82fd2d7ac2945f8c7189b0482012
Chris Wallwork <chris@unrefgugees.org>
2015-09-16 17:39:36 -0400

    added slider and infographics

--- _assets/javascripts/home.js
+++ _assets/javascripts/home.js
@@ -1,5 +1,30 @@
 'user-strict'
 
 $(document).ready(function(){
-  $('.banner').unslider();
+  
+  slider = $('.bxslider').bxSlider({
+    adaptiveHeight: true,
+    pager: false,
+    controls: false,
+  });
+
+  nextListener(slider);
+  prevListener(slider);
+
+  
 })
+
+function nextListener(slider){
+  $('.next-slide').click(function(event){
+    event.preventDefault();
+    console.log(slider);
+    slider.goToNextSlide();
+  });
+}
+
+function prevListener(slider){
+  $('.prev-slide').click(function(event){
+    event.preventDefault();
+    slider.goToPrevSlide();
+  });
+}
