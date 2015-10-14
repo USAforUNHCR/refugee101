3887682d9accf22742a7c5350329f31230bed31d
Chris Wallwork <chris@unrefgugees.org>
2015-10-14 11:55:17 -0400

    changed footer, added thank you page

--- _assets/javascripts/home.js
+++ _assets/javascripts/home.js
@@ -10,6 +10,7 @@ $(document).ready(function(){
 
   nextListener(slider);
   prevListener(slider);
+  submitEventListener();
 
   
 })
@@ -26,5 +27,41 @@ function prevListener(slider){
   $('.prev-slide').click(function(event){
     event.preventDefault();
     slider.goToPrevSlide();
+  });
+}
+
+function submitEventListener(){
+  $form = $("#petition");
+
+  $form.submit(function(event){
+    console.log(event);
+    event.preventDefault();
+    $('.submit-button').val('Please Wait...').prop('disabled',true);
+
+    $.ajax({
+      type: 'POST',
+      url: '//nyc.us11.list-manage.com/subscribe/post-json?u=7aa897cfc40f7cfbb83ffadd4&amp;id=c8e53459bc&c=?',
+      data: $form.serialize(),
+      timeout: 5000,
+      cache: false,
+      dataType: 'jsonp',
+      contentType: "application/json; charset=utf-8",
+      error: function(err) {console.log("Error.")},
+      success: function(data){
+        if (data.result != "success") {
+          $('.submit-button').val('Please Wait...').prop('disabled',true);
+          $('#conf-message').html('').slideUp(700);
+          $('#conf-message').html("Something went wrong, please try to submit your details again.").slideDown(700, function(){
+            $('.submit-button').val('Raise Your Hand!').prop('disabled',false);
+          });
+        }
+        else {
+          $('#petition').slideUp(700, function(){
+            $('#conf-message').html("Thanks! We sent you an email. Please confirm your email address to raise your hand for refugees.").slideDown(700);
+          });
+          
+        }
+      }
+    });
   });
 }
